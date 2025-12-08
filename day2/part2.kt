3e3d764437a3c7cbc276780c07e57a83e13a8a32
PieceOCode <geknirpst@arcor.de>
2025-12-08 22:49:21 +0100

    chore: make day2 part2 more kotlin idiomatic

--- day2/part2.kt
+++ day2/part2.kt
@@ -2,39 +2,30 @@ package day2.part1
 
 import java.io.File
 
+data class Interval(val start: Long, val end: Long)
+
 fun main() {
     val intervals = readInput()
-    var result = 0L
+    var result = intervals.asSequence()
+      .flatMap { (it.start..it.end).asSequence() }
+      .filter { isInvalidId(it) }
+      .sum()
 
-    for (interval in intervals) {
-        for (number:Long in interval.first..interval.second) {
-            if (isInvalidId(number)) {
-                result += number
-            }
-        }
-        println(result)
-    }
+    println(result)
 }
 
 fun isInvalidId(number: Number) : Boolean {
   val numberAsString = number.toString()
-  // println(number)
   for (i in 1..numberAsString.length/2) {
     val substring = numberAsString.substring(0, i)
-    var remainingString = numberAsString;
+    var remaining = numberAsString;
 
-    while (remainingString.length > 0) {
-      // println("${i}: ${substring} of ${remainingString}")
-
-      if (substring.length <= remainingString.length && substring == remainingString.substring(0, i)) {
-        remainingString = remainingString.substring(i)
-      } else {
-        break
-      }
+    while (remaining.length >= i && remaining.startsWith(substring)) {
+      remaining = remaining.substring(i)
     }
 
-    if (remainingString.length == 0) {
-      println("Invalid id! ${numberAsString}")
+    if (remaining.isEmpty()) {
+      // // println("Invalid id! ${numberAsString}")
       return true
     }
   }
@@ -42,12 +33,12 @@ fun isInvalidId(number: Number) : Boolean {
   return false 
 }
 
-fun readInput() : List<Pair<Long, Long>> {
+fun readInput() : List<Interval> {
     val inputFile = File("input.txt")
     val lines = inputFile.readLines()
     val numberPairs = lines.get(0).split(",")
 
     return numberPairs
         .map{ it.split("-") }
-        .map{ it[0].toLong() to it[1].toLong() }
+        .map{ Interval(it[0].toLong(), it[1].toLong()) }
 }
